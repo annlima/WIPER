c675ba27b4109eb4a98f3c3cf09241b78e7d4648
Andrea Lima <89811870+annlima@users.noreply.github.com>
2024-09-22 22:44:42 -0600

    Un poco del onboarding y permisos de la app

---
+++ WIPER/OnboardingViews/Carrousel.swift
@@ -0,0 +1,7 @@
+//
+//  Carrousel.swift
+//  WIPER
+//
+//  Created by Andrea Lima Blanca on 22/09/24.
+//
+
---
+++ WIPER/OnboardingViews/SplashScreen.swift
@@ -0,0 +1,41 @@
+import SwiftUI
+
+struct SplashScreen: View {
+    @State private var isAnimating = false
+    @State private var navigateToStartScreen = false
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color("BackgroundColor") // Fondo de la splash screen
+                    .edgesIgnoringSafeArea(.all)
+                
+                // Efecto de la cámara abriéndose con dos círculos
+                Circle()
+                    .fill(Color("Color"))
+                    .scaleEffect(isAnimating ? 3 : 0.1) // El círculo se expande
+                    .animation(.easeOut(duration: 1)) // Animación para abrir
+                    .onAppear {
+                        // Inicia la animación cuando aparece la vista
+                        withAnimation {
+                            isAnimating = true
+                        }
+                        // Desencadena la navegación después de 1 segundo
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1.2) {
+                            navigateToStartScreen = true
+                        }
+                    }
+                
+                NavigationLink(destination: StartDrivingScreen(), isActive: $navigateToStartScreen) {
+                    EmptyView()
+                }
+            }
+        }
+    }
+}
+
+struct SplashScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashScreen()
+    }
+}
